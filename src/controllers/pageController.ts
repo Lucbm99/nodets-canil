d330e5ad493f32749fa4038e79568679e1c6dcad
Lucbm99 <lbaradelmarchiori@gmail.com>
2021-08-21 10:37:43 -0300

    Divisão das rotas e fazendo os menus funcionarem

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,18 +1,40 @@
 import { Request, Response } from 'express';
 
+import { createMenuObject } from '../helpers/createMenuObject'
+
 export const home = (request: Request, response: Response) => {
-    response.send('home no controller!');
-    // response.render('pages/home');
+    response.render('pages/page', {
+        menu: createMenuObject('all'), 
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 }
 export const dogs = (request: Request, response: Response) => {
-    // response.render('pages/page');
-
+    response.render('pages/page', {
+        menu: createMenuObject('dog'), 
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 }
 export const cats = (request: Request, response: Response) => {
-    // response.render('pages/page');
-
+    response.render('pages/page', {
+        menu: createMenuObject('cat'), 
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 }
 export const fishes = (request: Request, response: Response) => {
-    // response.render('pages/page');
-
+    response.render('pages/page', {
+        menu: createMenuObject('fish'), 
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 }
