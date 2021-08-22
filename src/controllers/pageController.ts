8a007f1389c52f221dd2cd754b4f2f1eeccaa9db
Lucbm99 <lbaradelmarchiori@gmail.com>
2021-08-22 15:29:55 -0300

    Adição dos models e listagem dos animais no controller

--- src/controllers/pageController.ts
+++ src/controllers/pageController.ts
@@ -1,40 +1,53 @@
 import { Request, Response } from 'express';
 
+import { Pet } from '../models/pet';
 import { createMenuObject } from '../helpers/createMenuObject'
 
 export const home = (request: Request, response: Response) => {
+    let list = Pet.getAllAnimals();
+
     response.render('pages/page', {
         menu: createMenuObject('all'), 
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
-        }
+        },
+        list
     });
 }
 export const dogs = (request: Request, response: Response) => {
+    let list = Pet.getFromType('dog');
+
     response.render('pages/page', {
         menu: createMenuObject('dog'), 
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
-        }
+        }, 
+        list
     });
 }
 export const cats = (request: Request, response: Response) => {
+    let list = Pet.getFromType('cat');
+
     response.render('pages/page', {
         menu: createMenuObject('cat'), 
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
-        }
+        },
+        list
     });
 }
 export const fishes = (request: Request, response: Response) => {
+    let list = Pet.getFromType('fish');
+
     response.render('pages/page', {
         menu: createMenuObject('fish'), 
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
-        }
+        },
+        list
     });
 }
